b7a8a93fa3c83f6a869a7da6833b550985f22352
notgnirrac <carringtonmichael@gmail.com>
2025-07-08 20:26:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ async function render(fi, si) {
     let data = await response.json()
     console.log(data)
     // Your code goes here
+    fetch('https://rickandmortyapi.com/api/character/77,69')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
 }
 
 render(20, 35);
